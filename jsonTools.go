2f848710cd4750e27f18a6762a87ad4e435c2a75
ICfoxy <2786396952@qq.com>
2024-06-02 14:15:23 +0800

    20240602A\

--- jsonTools.go
+++ jsonTools.go
@@ -23,12 +23,8 @@ func RespondByJSON(w http.ResponseWriter, code int, input any) error {
 }
 
 // json格式报错
-func RespondByErr(w http.ResponseWriter, code int, errInfo string) error {
-	err := RespondByJSON(w, code, struct {
-		Err string
-	}{
-		Err: errInfo,
-	})
+func RespondByErr(w http.ResponseWriter, code int, err error) error {
+	err := RespondByJSON(w, code, err)
 	return err
 }
 
